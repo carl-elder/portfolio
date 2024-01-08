598c92ee80d8daa2bec31b1c4126e02fefd143a1
CarlBot-9000 <carl@carlelder.com>
2024-01-08 09:42:18 -0500

    further theme initial setup

--- wp-content/themes/celder_portfolio/functions.php
+++ wp-content/themes/celder_portfolio/functions.php
@@ -1,2 +1,28 @@
 <?php
 
+/**
+ * Enqueue theme stylesheet
+ */
+add_action( 'wp_enqueue_scripts', 'portfolio_enqueue_styles' );
+
+function portfolio_enqueue_styles() {
+    wp_enqueue_style(
+        'theme-slug-style',
+        get_stylesheet_uri()
+    );
+}
+
+/**
+ * Enqueue theme JS
+ */
+add_action( 'wp_enqueue_scripts', 'portfolio_scripts' );
+
+function portfolio_scripts() {
+    wp_enqueue_script(
+        'portfolio_script',
+        get_parent_theme_file_uri( 'inc/theme.js' ),
+        array(),
+        wp_get_theme()->get( 'Version' ),
+        true
+    );
+}
