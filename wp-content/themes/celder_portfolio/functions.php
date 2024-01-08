7ac71d0a3571ce41fad8ba9761c4702ac87b6a33
CarlBot-9000 <carl@carlelder.com>
2024-01-08 15:37:07 -0500

    beginning styles setup

--- wp-content/themes/celder_portfolio/functions.php
+++ wp-content/themes/celder_portfolio/functions.php
@@ -7,20 +7,35 @@ add_action( 'wp_enqueue_scripts', 'portfolio_enqueue_styles' );
 
 function portfolio_enqueue_styles() {
     wp_enqueue_style(
-        'theme-slug-style',
-        get_stylesheet_uri()
+        'portfolio-style',
+        get_stylesheet_uri(),
+        array(),
+        wp_get_theme()->get('Version'),
+        'all'
     );
+}
+
+/**
+ * Enqueue editor CSS
+ */
+
+
+add_action('after_setup_theme', 'portfolio_setup');
+
+function portfolio_setup() {
+    add_editor_style(get_stylesheet_uri());
+    get_theme_file_uri('inc/css/editor.css');
 }
 
 /**
  * Enqueue theme JS
  */
-add_action( 'wp_enqueue_scripts', 'portfolio_scripts' );
+add_action( 'wp_enqueue_scripts', 'portfolio_enqueue_scripts' );
 
-function portfolio_scripts() {
+function portfolio_enqueue_scripts() {
     wp_enqueue_script(
         'portfolio_script',
-        get_parent_theme_file_uri( 'inc/theme.js' ),
+        get_theme_file_path( 'inc/theme.js' ),
         array(),
         wp_get_theme()->get( 'Version' ),
         true
